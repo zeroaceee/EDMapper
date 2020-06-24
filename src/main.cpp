9945d7151914e2ae69269d52138e9ba13a3d8d65
zeroaceee <zeroaceee@gmail.com>
2020-06-24 13:21:34 +0200

    updated code.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 
 int main()
 {
+	g_process_id = Edmapper::GetProcessID("dummy.exe");
+	
+	if (g_process_id == 0)
+		return -1;
+
+	gProc_handle =  Edmapper::OpenProcessHandle(g_process_id);
+
+
 	if (Edmapper::GetRawDataFromFile("C:\\Users\\User\\Desktop\\cpp-projects\\EDMapper\\x64\\Release\\test.dll") != false)
 	{
-		std::wcout << "read dll." << '\n';
+		std::cout << "read dll." << '\n';
 	}
 		
 	
 	if (Edmapper::IsValidImage())
 	{
-		std::wcout << "Image is valid." << '\n';
+		std::cout << "Image is valid." << '\n';
 	}
 		
-
 
 	std::printf("Everything worked.\n");
 
--- src/pe/portable_executable.h
+++ src/pe/portable_executable.h
@@ -4,13 +4,16 @@
 
 namespace portable_exe{
 	inline bool IsValidImage();
+	inline void MapImageSections(std::vector<std::uint8_t> & image, PIMAGE_NT_HEADERS pnt_headers);
 }
 
-
+// make this shit private
+PIMAGE_DOS_HEADER pdos_header = nullptr;
+PIMAGE_NT_HEADERS pnt_headers = nullptr;
 
 bool portable_exe::IsValidImage()
 {
-	PIMAGE_DOS_HEADER pdos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(raw_data.data());
+	pdos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(raw_data.data());
 
 	if (pdos_header->e_magic != IMAGE_DOS_SIGNATURE)
 	{
@@ -18,13 +21,21 @@ bool portable_exe::IsValidImage()
 		return false;
 	}
 		
-	PIMAGE_NT_HEADERS pnt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(raw_data.data() + pdos_header->e_lfanew);
+	pnt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(raw_data.data() + pdos_header->e_lfanew);
 
 	if (pnt_headers->Signature != IMAGE_NT_SIGNATURE)
 	{
 		std::printf("[-]Invalid nt_headers signature.\n");
 		return false;
 	}
-
+	
 	return true;
+}
+
+
+void portable_exe::MapImageSections(std::vector<std::uint8_t>& image, PIMAGE_NT_HEADERS pnt_headers)
+{
+	for (int i = 0; i < pnt_headers->FileHeader.NumberOfSections; i++) {
+		// write sections to our target application.
+	}
 }
